03a4fa3899b67649bb58c0512d3b9f48246c1389
Michael Paffrath <michael.paffrath@gmail.com>
2022-02-16 01:43:59 +0100

    Remove sitesCommand

--- Configuration/Commands.php
+++ Configuration/Commands.php
@@ -1,9 +1,6 @@
 <?php
 
 return [
-    'sitemonitor:sites' => [
-        'class' => \LEPAFF\SiteMonitor\Command\SitesCommand::class,
-    ],
     'sitemonitor:updateclients' => [
         'class' => \LEPAFF\SiteMonitor\Command\UpdateClientsCommand::class,
     ]
