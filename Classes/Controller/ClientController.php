9c8f9fefefa4c5182aa1b540046e6b6d6e16bea9
Sabrina Zwirner <sabrina.zwirner@antwerpes.com>
2023-02-20 10:26:20 +0100

    add missing repository to controller

--- Classes/Controller/ClientController.php
+++ Classes/Controller/ClientController.php
@@ -1,31 +1,46 @@
 <?php
 
-    namespace LEPAFF\SiteMonitor\Controller;
+namespace LEPAFF\SiteMonitor\Controller;
 
-    use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ResponseInterface;
 
-    class ClientController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class ClientController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+{
+    /**
+     * clientRepository
+     *
+     * @var \LEPAFF\SiteMonitor\Domain\Repository\ClientRepository
+     */
+    protected $clientRepository = null;
+
+    /**
+     * @param \LEPAFF\SiteMonitor\Domain\Repository\ClientRepository $clientRepository
+     */
+    public function injectClientRepository(\LEPAFF\SiteMonitor\Domain\Repository\ClientRepository $clientRepository)
     {
-        /**
-         * action new
-         *
-         * @return string|object|null|void
-         */
-        public function newAction() : ResponseInterface
-        {
-            return $this->htmlResponse();
-        }
-
-        /**
-         * action create
-         *
-         * @param \LEPAFF\SiteMonitor\Domain\Model\Client $newClient
-         * @return string|object|null|void
-         */
-        public function createAction(\LEPAFF\SiteMonitor\Domain\Model\Client $newClient)
-        {
-            $this->addFlashMessage('The object was created. Please be aware that this action is publicly accessible unless you implement an access check. See https://docs.typo3.org/p/friendsoftypo3/extension-builder/master/en-us/User/Index.html', '', \TYPO3\CMS\Core\Messaging\AbstractMessage::WARNING);
-            $this->clientRepository->add($newClient);
-            $this->redirect('list');
-        }
+        $this->clientRepository = $clientRepository;
     }
+
+    /**
+     * action new
+     *
+     * @return string|object|null|void
+     */
+    public function newAction(): ResponseInterface
+    {
+        return $this->htmlResponse();
+    }
+
+    /**
+     * action create
+     *
+     * @param \LEPAFF\SiteMonitor\Domain\Model\Client $newClient
+     * @return string|object|null|void
+     */
+    public function createAction(\LEPAFF\SiteMonitor\Domain\Model\Client $newClient)
+    {
+        $this->addFlashMessage('The object was created. Please be aware that this action is publicly accessible unless you implement an access check. See https://docs.typo3.org/p/friendsoftypo3/extension-builder/master/en-us/User/Index.html', '', \TYPO3\CMS\Core\Messaging\AbstractMessage::WARNING);
+        $this->clientRepository->add($newClient);
+        $this->redirect('list');
+    }
+}
